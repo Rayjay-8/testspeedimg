e87abf773c0b70321fd04a924a3ddaf67ad94f4f
rayjay-8 <rayarruda9876@gmail.com>
2025-04-19 10:58:38 -0300

    add bunny

--- speedTest.js
+++ speedTest.js
@@ -6,7 +6,7 @@ const imageUrls = [
   { name: "Vercel Blob", url: "https://6w8od0naqkkwxclw.public.blob.vercel-storage.com/img-rDQH15RGcmG00WYlmlMIh3igypaOXO.png" },
   // { name: "Supabase", url: "https://dupdfizlkrggeqeivwha.supabase.co/storage/v1/object/public/publico//img.png" },
   { name: "Cloudflare R2", url: "https://pub-5635a0fb0112407381937ce5f8906cb9.r2.dev/img.png" },
-  { name: "bunny", url: "https://6w8od0naqkkwxclw.public.blob.vercel-storage.com/img-rDQH15RGcmG00WYlmlMIh3igypaOXO.png"}
+  { name: "bunny", url: "https://gitfull.b-cdn.net/img.png"}
 ];
 
 async function testImageSpeed(urls) {
